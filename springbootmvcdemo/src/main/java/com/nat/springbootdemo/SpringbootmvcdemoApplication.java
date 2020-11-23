c7b7516d56c22b809b2dc0c1a24dca4d7a50b7cb
nat <nmexodus870@gmail.com>
2020-11-23 20:21:11 +0000

    spring boot mvc

---
+++ springbootmvcdemo/src/main/java/com/nat/springbootdemo/SpringbootmvcdemoApplication.java
@@ -0,0 +1,13 @@
+package com.nat.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmvcdemoApplication.class, args);
+	}
+
+}
---
+++ springbootmvcdemo/src/test/java/com/nat/springbootdemo/SpringbootmvcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nat.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmvcdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
